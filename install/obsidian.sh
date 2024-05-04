60b615626103fa4209abb64220110d0276530a12
Martin <martinft@t-online.de>
2024-05-05 00:51:50 +0200

    Install Obsidian

---
+++ install/obsidian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo "Installing Obsidian"
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/obsidian_1.5.12_amd64.deb
+sudo apt install ./obsidian_1.5.12_amd64.deb
+
