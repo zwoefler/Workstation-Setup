6dd533973506da60285688d5cd2a777870e601f2
Martin <martinft@t-online.de>
2024-05-05 00:09:24 +0200

    Udpating deploy ubuntu script

--- deploy_ubuntu.sh
+++ deploy_ubuntu.sh
@@ -1,24 +1,61 @@
 #!/bin/bash
 
-echo "Creating SSH Key for $USER at ~/.ssh/id_rsa"
-ssh-keygen -t rsa -b 4096 -f "$HOME/.ssh/id_rsa" -N ""
+print_help() {
+    echo "Usage: $0 [options]"
+    echo "No options provided executes the base routine"
+    echo ""
+    echo "Options:"
+    echo "  config      Install only configuration files"
+    echo "  --help, -h  Show this help message"
+    echo ""
+}
 
-echo "Updating apt packages"
-sudo apt update
+install_config_files(){
+    echo "Installing configuration files..."
+    echo "VIM"
+    wget -O ~/.bashrc https://raw.githubusercontent.com/zwoefler/Dot-File-Repo/master/bash/.bashrc
+    echo "BASHRC"
+    wget -O ~/.vimrc https://raw.githubusercontent.com/zwoefler/Dot-File-Repo/master/vim/.vimrc
+    echo "Configuration files installed successfully."
+    exit 0
+}
 
+create_ssh_key(){
+    if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+        echo "Creating SSH Key for $USER at ~/.ssh/id_rsa"
+        ssh-keygen -t rsa -b 4096 -f "$HOME/.ssh/id_rsa" -N ""
+    else
+        echo "SSH Key already exists. Skipping creation."
+    fi
+}
 
-echo "Installing dotfiles for"
-echo "VIM"
-wget -O ~/.bashrc https://raw.githubusercontent.com/zwoefler/Dot-File-Repo/master/bash/.bashrc
-echo "BASHRC"
-wget -O ~/.vimrc https://raw.githubusercontent.com/zwoefler/Dot-File-Repo/master/vim/.vimrc
+install_base() {
+    echo "Updating apt packages"
+    sudo apt update
 
+    create_ssh_key
+    install_config_files
+    
+    echo "Installing python3-pip"
+    sudo apt install -y python3-pip
+    
+    echo "Removing SNAP"
+    sudo apt remove -y --purge snapd
+    
+    echo "Sourcing .bashrc"
+    source ~/.bashrc
+}
 
-echo "Installing python3-pip"
-sudo apt install -y python3-pip
+# Check for command line options
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    print_help
+    exit 0
+fi
 
-echo "Removing SNAP"
-sudo apt remove -y --purge snapd
+# If config option is provided, install only config files
+if [ "$1" == "config" ]; then
+    install_config_files
+    exit 0
+fi
 
-echo "Sourcing .bashrc"
-source ~/.bashrc
+install_base
