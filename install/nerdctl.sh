58718fbe0d9fafe8a8923c7f496c6ceb5508d807
Martin <martinft@t-online.de>
2024-05-04 23:03:46 +0200

    Nerdctl uninstall option and variable version

--- install/nerdctl.sh
+++ install/nerdctl.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+
+NERDCTL_VERSION=1.7.6
+
 install_nerdctl() {
-    echo "Installing Rootless nerdctl"
+    echo "Installing Rootless nerdctli version: $NERDCTL_VERSION"
     echo "Checking requirement uidmap package is installed"
     if ! dpkg -l | grep -q uidmap; then
         echo "uidmap not installed. Installing..."
@@ -10,14 +13,27 @@ install_nerdctl() {
     fi
 
     echo "Installing nerdctl"
-    wget https://github.com/containerd/nerdctl/releases/download/v1.7.6/nerdctl-full-1.7.6-linux-amd64.tar.gz
-    sudo tar Cxzvvf /usr/local nerdctl-full-1.7.6-linux-amd64.tar.gz
+    wget https://github.com/containerd/nerdctl/releases/download/v$NERDCTL_VERSION/nerdctl-full-$NERDCTL_VERSION-linux-amd64.tar.gz
+    sudo tar Cxzvvf /usr/local nerdctl-full-$NERDCTL_VERSION-linux-amd64.tar.gz
     containerd-rootless-setuptool.sh install
     echo "Successfully installed NERDCTL"
 }
 
 uninstall_nerdctl() {
-    echo "TBD" 
+    echo "Uninstalling nerdctl, requirered packages and containerd" 
+    sudo apt remove -y uidmap
+
+    echo "Removing Containerd and nerdctl"
+    # Stolen from
+    # 1nachi on GITHUB GIST: https://gist.github.com/1nachi
+    # https://gist.github.com/1nachi/3d283a216481078c147fce784fdbecc6#file-containerd-purge-sh    
+    sudo rm -v /usr/local/bin/{containerd*,buildctl,buildkitd,bypass4*,ctd-decoder,ctr*,fuse-overlayfs,ipfs,nerdctl,rootlessctl,rootlesskit,runc,slirp4netns,tini}
+    sudo rm -v /usr/local/lib/systemd/system/{containerd.service,buildkit.service,stargz-snapshotter.service}
+    sudo rm -rv /opt/{cni,containerd}
+    sudo rm -v /usr/local/sbin/runc
+    sudo rm -rv $XDG_RUNTIME_DIR/{buildkit,containerd-rootless}
+    sudo rm -rv /usr/local/share/doc/nerdctl*
+    sudo rm -v /etc/bash_completion.d/nerdctl
 }
 
 
