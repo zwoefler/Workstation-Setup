f5c1d4649902fae93fa75e0e36b4d969ee4ccd73
Martin <martinft@t-online.de>
2024-05-05 12:37:39 +0200

    Adding steam install

---
+++ install/steam.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+install() {
+    echo "Installing Steam"
+    echo "Install Steam on Ubuntu via Steam APT Repository"
+    echo "Importing GPG key"
+    curl -s http://repo.steampowered.com/steam/archive/stable/steam.gpg | sudo gpg --dearmor -o /usr/share/keyrings/steam.gpg > /dev/null
+
+    echo "Add steam apt repository"
+    echo deb [arch=amd64 signed-by=/usr/share/keyrings/steam.gpg] http://repo.steampowered.com/steam/ stable steam | sudo tee /etc/apt/sources.list.d/steam.list
+
+    echo "Update apt cache after steam PPA import"
+    sudo apt update
+
+    echo "Install Steam via package manager"
+    sudo apt-get install \
+      libgl1-mesa-dri:amd64 \
+      libgl1-mesa-dri:i386 \
+      libgl1-mesa-glx:amd64 \
+      libgl1-mesa-glx:i386 \
+      steam-launcher
+
+    echo "Remove steam extra source list - redundant lists"
+    sudo rm /etc/apt/sources.list.d/steam-beta.list
+    sudo rm /etc/apt/sources.list.d/steam-stable.list
+
+    echo "Refresh apt index"
+    sudo apt update
+}
+
+
+uninstall() {
+    echo "Uninstalling Steam"
+}
+
+
+# Check if uninstall option is provided
+if [ "$1" == "uninstall" ]; then
+    uninstall
+    exit 0
+fi
+
+if command -v steam &> /dev/null; then
+    echo -e "Steam is already installed."
+else
+    install
+fi
