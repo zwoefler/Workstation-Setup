22271215ddb36b853375d148a92f220c686bab06
Martin <martinft@t-online.de>
2024-05-05 12:27:49 +0200

    Adding uninstall and install options to obsidian script

--- install/obsidian.sh
+++ install/obsidian.sh
@@ -2,7 +2,37 @@
 
 OBSIDIAN_VERSION=1.5.12
 
-echo "Installing Obsidian"
-wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
-sudo apt install ./obsidian_${OBSIDIAN_VERSION}_amd64.deb
 
+
+install() {
+    echo "Installing Obsidian"
+    wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+    sudo apt install ./obsidian_${OBSIDIAN_VERSION}_amd64.deb
+    rm ./obsidian_${OBSIDIAN_VERSION}_amd64.deb
+}
+
+
+pull_notes_from_github(){
+    echo "Pull notes from GitHub"
+    # TBD
+}
+
+uninstall() {
+    echo "Uninstalling Obsidian"
+    sudo apt remove -y obsidian
+
+    # Should notes directory/config also be removed?
+}
+
+
+# Check if uninstall option is provided
+if [ "$1" == "uninstall" ]; then
+    uninstall
+    exit 0
+fi
+
+if command -v obsidian &> /dev/null; then
+    echo -e "obsidian is already installed."
+else
+    install
+fi
