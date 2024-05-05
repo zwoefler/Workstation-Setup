820729b6366b58f6c97482e9bc511f761e62887c
Martin <martinft@t-online.de>
2024-05-05 12:20:53 +0200

    Fix version number variable Obsidina install

--- install/obsidian.sh
+++ install/obsidian.sh
@@ -3,6 +3,6 @@
 OBSIDIAN_VERSION=1.5.12
 
 echo "Installing Obsidian"
-wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v$OBSIDIAN_VERSION/obsidian_$OBSIDIAN_VERSION_amd64.deb"
-sudo apt install ./obsidian_$OBSIDIAN_VERSION_amd64.deb
+wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v${OBSIDIAN_VERSION}/obsidian_${OBSIDIAN_VERSION}_amd64.deb"
+sudo apt install ./obsidian_${OBSIDIAN_VERSION}_amd64.deb
 
