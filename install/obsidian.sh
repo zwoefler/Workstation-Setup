a1812df2740fe4060494c6f4fb996137de2bcfef
Martin <martinft@t-online.de>
2024-05-05 12:17:47 +0200

    Adding version as variable to Obsidian install

--- install/obsidian.sh
+++ install/obsidian.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+OBSIDIAN_VERSION=1.5.12
 
 echo "Installing Obsidian"
-wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.12/obsidian_1.5.12_amd64.deb
-sudo apt install ./obsidian_1.5.12_amd64.deb
+wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v$OBSIDIAN_VERSION/obsidian_$OBSIDIAN_VERSION_amd64.deb"
+sudo apt install ./obsidian_$OBSIDIAN_VERSION_amd64.deb
 
